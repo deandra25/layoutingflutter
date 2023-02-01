5ee70adb2c4bf51347df746c6b79c2934f89d246
Revan Rionaldo <91299304+revanrion08@users.noreply.github.com>
2023-02-01 11:30:13 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Belajar Layout"),
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            color: Colors.greenAccent,
+            child: FlutterLogo(
+              size: 60.0,
+            ),
+          ),
+          Container(
+            color: Colors.orangeAccent,
+            child: FlutterLogo(
+              size: 60.0,
+            ),
+          ),
+          Container(
+            color: Colors.purpleAccent,
+            child: FlutterLogo(
+              size: 60.0,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
